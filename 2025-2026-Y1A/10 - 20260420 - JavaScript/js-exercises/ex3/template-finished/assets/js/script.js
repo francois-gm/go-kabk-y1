657870a47a0d0abba1b2fa66b10c2534c345a403
francois-gm <francois.girardm@gmail.com>
2026-03-24 19:31:04 +0100

    commit

--- 2025-2026-Y1A/10 - 20260420 - JavaScript/js-exercises/ex3/template-finished/assets/js/script.js
+++ 2025-2026-Y1A/10 - 20260420 - JavaScript/js-exercises/ex3/template-finished/assets/js/script.js
@@ -4,22 +4,10 @@
 
 const btnDarkMode = document.querySelector(".btn-dark-mode");
 
-const menu = document.querySelector("nav.menu");
-const btnModalToggle = document.querySelector(".btn-modal-toggle");
-const btnModalClose = document.querySelector(".btn-modal-close");
-
 // then declare your event listeners
 
 btnDarkMode.addEventListener("click", function () {
   document.body.classList.toggle('dark-mode');
-});
-
-btnModalToggle.addEventListener("click", function () {
-  menu.classList.toggle('modal-is-open');
-});
-
-btnModalClose.addEventListener("click", function () {
-  menu.classList.remove('modal-is-open');
 });
 
 
