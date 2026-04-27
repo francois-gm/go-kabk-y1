3659f5dce47c235ce7b23b618554fa36417825f0
francois-gm <francois.girardm@gmail.com>
2026-04-27 13:03:04 +0200

    commit

--- 2025-2026-Y1A/10 - 20260420 - JavaScript/js-exercises/ex3/template-setup/assets/js/script.js
+++ 2025-2026-Y1A/10 - 20260420 - JavaScript/js-exercises/ex3/template-setup/assets/js/script.js
@@ -2,12 +2,8 @@
 
 // always declare your variables first
 
-const btnDarkMode = document.querySelector(".btn-dark-mode");
+
 
 // then declare your event listeners
-
-btnDarkMode.addEventListener("click", function () {
-  document.body.classList.toggle('dark-mode');
-});
 
 
